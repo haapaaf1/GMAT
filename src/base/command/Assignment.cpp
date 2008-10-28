949be68c1ea7bd1f76eff714eae9a63bd270f822
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2008-10-28 17:33:49 +0000

    Just pass true for setRefObj to ElementWrapper::SetValue(). It broke some other function tests. Need to come back to fix Func_LibrationSE sctipt.

--- src/base/command/Assignment.cpp
+++ src/base/command/Assignment.cpp
@@ -636,23 +636,32 @@ bool Assignment::Execute()
    try
    {
       bool retval = false;
-      
-      // Determine if ref object shoud be set to lhs (loj: 2008.10.24)
-      // To handle setting spacecraft's CoordinateSystem which is not NULL and
+
+      // In attempt to fix Func_LibrationSE.script for nested (loj: 2008.10.24)
+      // function which doesn't give correct results.
+      // We need to determine if ref object shoud be set to lhs to handle setting
+      // spacecraft's CoordinateSystem which is not NULL and
       // CoordinateSystem is defined later in the GmatFunction script.
       // GMAT Sat1.CoordinateSystem = EarthSunL1_MJ2000Eq;
       // Since Spacecraft converts initial state to given CoordinateSyatem
       // when CoordinateSyatem is not NULL for nested GmatFunction,
       // we don't want to convert until initial state is set.
       // So pass setRefObj to true when this command is not in GmatFunction and
-      // if both lhs and rhs are whole objects
-      bool setRefObj = false;
-      if (currentFunction == NULL)
-         setRefObj = true;
-      else
-         if (lhsWrapper->GetWrapperType() == Gmat::OBJECT &&
-             lhsWrapper->GetWrapperType() == lhsWrapper->GetWrapperType())
-            setRefObj = true;
+      // if both lhs and rhs are whole objects.
+      
+      // @todo This code broke routine test script APT_Func_allPropagates on 2nd run,
+      // so set setRefObj to true for now
+      
+      //bool setRefObj = false;
+      //if (currentFunction == NULL)
+      //   setRefObj = true;
+      //else
+      //   if (lhsWrapper->GetWrapperType() == Gmat::OBJECT &&
+      //       lhsWrapper->GetWrapperType() == rhsWrapper->GetWrapperType())
+      //      setRefObj = true;      
+      //MessageInterface::ShowMessage("===> setRefObj=%d\n", setRefObj);
+      
+      bool setRefObj = true;
       
       // Use ElementWrapper static method SetValue() (loj: 2008.06.20)
       if (mathTree == NULL)
