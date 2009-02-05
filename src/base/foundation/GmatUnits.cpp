63f285d4f48d1b5d1118a0126618a9a44399159e
aero9600 <aero9600@222415a0-0dc2-4184-858d-a27cde375526>
2009-02-05 01:29:24 +0000

    A few updates.

--- src/base/foundation/GmatUnits.cpp
+++ src/base/foundation/GmatUnits.cpp
@@ -7,20 +7,20 @@
  const std::string GmatUnits::SI_BASE_UNITS_STRING[UNKNOWN_BASE_UNIT+1] =
 {
     "Length", "Mass", "Time", "ElectricCurrent", "Temperature",
-    "AmountOfSubstance", "LuminousIntensity", "UnknownBaseUnit"
+    "AmountOfSubstance", "LuminousIntensity", "", "UnknownBaseUnit"
 
 };
 
  const std::string GmatUnits::SI_BASE_UNITS_SYMBOL[UNKNOWN_BASE_UNIT+1] =
 {
-    "L","M","T","A","K","MOL","C",""
+    "L","M","T","A","K","MOL","C","",""
 };
 
 
  const Real GmatUnits::SI_PREFIXES_FACTOR[UNKNOWN_SI_PREFIX+1] =
 {
   1e24,     1e21,   1e18,   1e15,   1e12,   1e9,    1e6,    1000.0,
-  100.0,    10.0,   0.1,    0.01,   0.001,  1e-6,   1e-9,   1e-12,
+  100.0,    10.0,   1, 0.1,    0.01,   0.001,  1e-6,   1e-9,   1e-12,
   1e-15,    1e-18,  1e-21,  1e-24,  1.0
 };
 
@@ -131,7 +131,9 @@ GmatUnits::ENERGY_UNITS_STRING[UNKNOWN_ENERGY_UNIT+1] =
 = 4.4482216152605 N (exactly)
 
            newton	dyne            kilopond	 pound-force	poundal
-1 N	≡ 1 kg·m/s²	= 105 dyn	≈ 0.10197 kp	≈ 0.22481 lbf	≈ 7.2330 pdl
+
+const Real GmatUnits::FORCE_UNITS_CONVERSION[UNKNOWN_FORCE_UNIT+1] =
+ 1N	≡ 1 kg·m/s²	= 105 dyn	≈ 0.10197 kp	≈ 0.22481 lbf	≈ 7.2330 pdl
 1 dyn	= 10−5 N	≡ 1 g·cm/s²	≈ 1.0197×10−6 kp ≈ 2.2481×10−6 lbf	≈ 7.2330×10−5 pdl
 1 kp	= 9.80665 N	= 980665 dyn	≡ gn·(1 kg)	≈ 2.2046 lbf	≈ 70.932 pdl
 1 lbf	≈ 4.448222 N	≈ 444822 dyn	≈ 0.45359 kp	≡ gn·(1 lb)	≈ 32.174 pdl
@@ -139,18 +141,31 @@ GmatUnits::ENERGY_UNITS_STRING[UNKNOWN_ENERGY_UNIT+1] =
 
  */
 
+const Real GmatUnits::FORCE_UNITS_CONVERSION[UNKNOWN_FORCE_UNIT+1][UNKNOWN_FORCE_UNIT+1] =
+{
+    1, 105, 0.10197, 0.22481, 7.2330, 0, 0, 0, 0,
+    0.00001,	1, 1.0197e-6, 2.2481e-6, 7.2330e-5, 0, 0, 0, 0,
+    9.80665, 980665, 1, 2.2046, 70.932, 0, 0, 0, 0,
+    4.448222, 444822, 0.45359, 1, 32.174, 0, 0, 0, 0,
+    0.138255, 13825, 0.014098, 0.031081, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
  const std::string
 GmatUnits::FORCE_UNITS_SYMBOL[UNKNOWN_FORCE_UNIT+1] =
 {
-    "dyn", "kp", "kip", "N", "Planck-force", "lbf", "pdl",  "sn",
+    "N","dyn", "kp", "lbf", "pdl", "kip", "Planck-force",  "sn",
     "UnknownForceUnit"
 };
 
  const std::string
 GmatUnits::FORCE_UNITS_STRING[UNKNOWN_FORCE_UNIT+1] =
 {
-    "Dyne", "Kilopond", "Kip", "Newton", "Planck-force",
-    "Pound-force", "Poundal", "Sthene",  "UnknownForceUnit"
+    "Newton", "Dyne", "Kilopond", "Pound-force", "Poundal", "Kip",
+    "Planck-force", "Sthene",  "UnknownForceUnit"
 };
 
  const std::string
@@ -346,3 +361,191 @@ GmatUnits::CANADA_TIME_UTC_OFFSETS[EndCanadaTimeZones+1] =
     -3.5, -4.0, -5.0, -6.0, -7.0, -8.0, -8.0,
     -2.5, -3.0, -4.0, -5.0, -6.0, -7.0,  0.0
 };
+
+
+//---------------------------------------------------------------------------
+//  GmatUnits(const std::string &myUnits)
+//---------------------------------------------------------------------------
+/**
+ * Constructs base GmatUnits structures used in GMAT extensible classes.
+ *
+ * @param <myUnits> String unit keyword such as velocity, force, etc
+ */
+//---------------------------------------------------------------------------
+GmatUnits::GmatUnits(const std::string &myUnits) :
+    unitType (myUnits)
+{
+    // Initialize rest of unit information here
+}
+
+//---------------------------------------------------------------------------
+//  GmatUnits(const StringArray &numerator, const StringArray &denominator)
+//---------------------------------------------------------------------------
+/**
+ * Constructs base GmatUnits structures used in GMAT extensible classes.
+ *
+ * @param <numerator> String array containing base SI units in numerator
+ * @param <denominator> String array containing base SI units in denominator
+ */
+//---------------------------------------------------------------------------
+GmatUnits::GmatUnits(const StringArray &numerator,
+                     const StringArray &denominator) :
+    unitNumerator(numerator), unitDenominator(denominator)
+{
+    // Initialize rest of unit information here
+}
+
+//---------------------------------------------------------------------------
+//  ~GmatUnits()
+//---------------------------------------------------------------------------
+/**
+ * Destructor
+ */
+//---------------------------------------------------------------------------
+~GmatUnits::GmatUnits()
+{
+}
+
+//---------------------------------------------------------------------------
+//  GmatUnits(const GmatUnits &units)
+//---------------------------------------------------------------------------
+/**
+ * Copy Constructor for base GmatUnits structures.
+ *
+ * @param <units> The original that is being copied.
+ */
+//---------------------------------------------------------------------------
+GmatUnits::GmatUnits(const GmatUnits &units) :
+    unitType (units.unitType),
+    displayUnits (units.displayUnits),
+    conversionFactor (units.conversionFactor),
+    numeratorUnits (units.numeratorUnits),
+    denominatorUnits (units.denominatorUnits),
+    numeratorPowers (units.numeratorPowers),
+    denominatorPowers (units.denominatorPowers),
+    numeratorPrefix (units.numeratorPrefix),
+    denominatorPrefix (units.denominatorPrefix)
+{
+       
+}
+
+//---------------------------------------------------------------------------
+//  GmatUnits& operator=(const GmatUnits &units)
+//---------------------------------------------------------------------------
+/**
+ * Assignment operator for GmatUnits structures.
+ *
+ * @param <units> The original that is being copied.
+ *
+ * @return Reference to this object
+ */
+//---------------------------------------------------------------------------
+GmatUnits& GmatUnits::GmatUnits& operator=(const GmatUnits &units)
+{
+   // Don't do anything if copying self
+   if (&units == this)
+      return *this;
+
+    unitType = units.unitType;
+    displayUnits = units.displayUnits;
+    conversionFactor = units.conversionFactor;
+    numeratorUnits = units.numeratorUnits;
+    denominatorUnits = units.denominatorUnits;
+    numeratorPowers = units.numeratorPowers;
+    denominatorPowers = units.denominatorPowers;
+    numeratorPrefix = units.numeratorPrefix;
+    denominatorPrefix = units.denominatorPrefix);
+
+    return *this;
+
+}
+
+
+//---------------------------------------------------------------------------
+//  std::string GetUnitType() const
+//---------------------------------------------------------------------------
+/**
+ * Retrieves the current unit type (length, velocity, force, etc).
+ *
+ * @return The current unit type.
+ */
+//---------------------------------------------------------------------------
+std::string GmatUnits::GetUnitType() const
+{
+    return unitType;
+}
+
+//---------------------------------------------------------------------------
+//  void SetUnitType(const std::string type)
+//---------------------------------------------------------------------------
+/**
+ * Set the desired standard unit type (length, velocity, force, etc).
+ *
+ * @param <type> The desired standard unit type.
+ *
+ */
+//---------------------------------------------------------------------------
+void GmatUnits::SetUnitType(const std::string type)
+{
+    unitType = type;
+}
+
+//---------------------------------------------------------------------------
+//  std::string GetDisplayUnits() const
+//---------------------------------------------------------------------------
+/**
+ * Retrieves the current unit text formatted for display.
+ *
+ * @return The current display unit text.
+ */
+//---------------------------------------------------------------------------
+std::string GmatUnits::GetDisplayUnits() const
+{
+    return displayUnits;
+}
+
+//---------------------------------------------------------------------------
+//  void SetDisplayUnits(const std::string text)
+//---------------------------------------------------------------------------
+/**
+ * Set the desired display unit text.
+ *
+ * @param <text> The desired unit text formatted for display.
+ *
+ */
+//---------------------------------------------------------------------------
+void GmatUnits::SetDisplayUnits(const std::string text)
+{
+    displayUnits = text;
+}
+
+//---------------------------------------------------------------------------
+//  Real GetConversionFactor() const
+//---------------------------------------------------------------------------
+/**
+ * Retrieves the current conversion factor value.
+ *
+ * @return The current conversion factor.
+ *
+ */
+//---------------------------------------------------------------------------
+Real GmatUnits::GetConversionFactor() const
+{
+    return conversionFactor;
+}
+
+//---------------------------------------------------------------------------
+// void SetConversionFactor(Real factor)
+//---------------------------------------------------------------------------
+/**
+ * Sets the conversion factor to the desired value.
+ *
+ * @param <factor> The desired conversion factor value.
+ *
+ */
+//---------------------------------------------------------------------------
+void GmatUnits::SetConversionFactor(Real factor)
+{
+    conversionFactor = factor;
+}
+
--- src/base/foundation/GmatUnits.hpp
+++ src/base/foundation/GmatUnits.hpp
@@ -26,6 +26,14 @@ class GMAT_API GmatUnits : public GmatBase
 {
 
 public:
+
+    // The usual suspects
+    GmatUnits(const std::string &myUnits);
+    GmatUnits(const StringArray &numerator, const StringArray &denominator);
+    ~GmatUnits();
+    GmatUnits(const GmatUnits &units);
+    GmatUnits& operator=(const GmatUnits &units);
+
     
    /**
     * The list of SI Prefixes
@@ -43,6 +51,7 @@ public:
       KILO,
       HECTO,
       DEKA,
+      NOPREFIX,
       DECI,
       CENTI,
       MILLI,
@@ -74,15 +83,15 @@ public:
     */
    enum AreaUnits
    {
-      SQUARE_INCH,
-      SQUARE_FOOT,
-      SQUARE_YARD,
-      SQUARE_ROD,
-      SQUARE_MILE,
-      ACRE,
-      SQUARE_METER,
-      ARE,
-      HECTARE,
+      AU_SQUARE_INCH,
+      AU_SQUARE_FOOT,
+      AU_SQUARE_YARD,
+      AU_SQUARE_ROD,
+      AU_SQUARE_MILE,
+      AU_ACRE,
+      AU_SQUARE_METER,
+      AU_ARE,
+      AU_HECTARE,
       UNKNOWN_AREA_UNIT
    };
 
@@ -92,20 +101,20 @@ public:
     */
    enum DistanceUnits
    {
-      METER,
-      LEAGUE,
-      MILE,
-      NAUTICAL_MILE,
-      FURLONG,
-      ROD,
-      FATHOM,
-      YARD,
-      FOOT,
-      INCH,
-      ASTRONOMICAL_UNIT,
-      LIGHT_YEAR,
-      PARSEC,
-      ANGSTROM,
+      DU_METER,
+      DU_LEAGUE,
+      DU_MILE,
+      DU_NAUTICAL_MILE,
+      DU_FURLONG,
+      DU_ROD,
+      DU_FATHOM,
+      DU_YARD,
+      DU_FOOT,
+      DU_INCH,
+      DU_ASTRONOMICAL_UNIT,
+      DU_LIGHT_YEAR,
+      DU_PARSEC,
+      DU_ANGSTROM,
       UNKNOWN_DISTANCE_UNIT
    };
 
@@ -115,14 +124,14 @@ public:
     */
    enum ElectricUnits
    {
-      MY_ELECTRIC_CURRENT,
-      ELECTRIC_CHARGE,
-      SI_ELECTRIC_CHARGE,
-      ELECTRIC_CAPACITANCE,
-      ELECTRIC_CONDUCTANCE,
-      ELECTRIC_INDUCTANCE,
-      ELECTRIC_POTENTIAL_DIFFERENCE,
-      ELECTRIC_RESISTANCE,
+      EU_CURRENT,
+      EU_CHARGE,
+      EU_SI_CHARGE,
+      EU_CAPACITANCE,
+      EU_CONDUCTANCE,
+      EU_INDUCTANCE,
+      EU_POTENTIAL_DIFFERENCE,
+      EU_RESISTANCE,
       UNKNOWN_ELECTRIC_UNIT
    };
 
@@ -132,14 +141,14 @@ public:
     */
    enum EnergyUnits
    {
-      WAVENUMBER,
-      JOULES,
-      JOULES_PER_MOLE,
-      CALORIES,
-      CALORIES_PER_MOLE,
-      NANOMETER,
-      HERTZ,
-      ELECTRON_VOLT,
+      ENU_WAVENUMBER,
+      ENU_JOULES,
+      ENU_JOULES_PER_MOLE,
+      ENU_CALORIES,
+      ENU_CALORIES_PER_MOLE,
+      ENU_NANOMETER,
+      ENU_HERTZ,
+      ENU_ELECTRON_VOLT,
       UNKNOWN_ENERGY_UNIT
    };
 
@@ -149,14 +158,14 @@ public:
     */
    enum ForceUnits
    {
-      DYNE,
-      KILOGRAM_FORCE,
-      KIP,
-      NEWTON,
-      PLANCK_FORCE,
-      POUND_FORCE,
-      POUNDAL,
-      STHENE,
+      FU_NEWTON,
+      FU_DYNE,
+      FU_KILOGRAM_FORCE,
+      FU_POUND_FORCE,
+      FU_POUNDAL,
+      FU_KIP,
+      FU_PLANCK_FORCE,
+      FU_STHENE,
       UNKNOWN_FORCE_UNIT
    };
 
@@ -167,38 +176,38 @@ public:
     */
    enum LuminanceUnits
    {
-      LAMBERT,
-      FOOT_LAMBERT,
-      CANDELA_PER_SQUARE_METER,
-      NIT,
+      LU_LAMBERT,
+      LU_FOOT_LAMBERT,
+      LU_CANDELA_PER_SQUARE_METER,
+      LU_NIT,
       UNKNOWN_LUMINANCE_UNIT
    };
 
    enum IlluminanceUnits
    {
-       FOOT_CANDLE,
-       LUX,
-       PHOT,
+       IU_FOOT_CANDLE,
+       IU_LUX,
+       IU_PHOT,
        UNKNOWN_ILLUMINANCE_UNIT
    };
 
-   enum LuminousFlux
+   enum LuminousFluxUnits
    {
-       LUMEN,
-       RAYLEIGH,
+       LFU_LUMEN,
+       LFU_RAYLEIGH,
        UNKNOWN_LUMINOUSFLUX_UNIT
    };
 
-   enum LuminousEnergy
+   enum LuminousEnergyUnits
    {
-       TALBOT,
+       LEU_TALBOT,
        UNKNOWN_LUMINOUSENERGY_UNIT
    };
 
-   enum LuminousIntensity
+   enum LuminousIntensityUnits
    {
-       CANDELA,
-       CANDELPOWER,
+       LIU_CANDELA,
+       LIU_CANDELPOWER,
        UNKNOWN_LUMINOUSINTENSITY_UNIT
    };
 
@@ -208,15 +217,15 @@ public:
     */
    enum RadianceUnits
    {
-      RADIANT_ENERGY,
-      RADIANT_FLUX,
-      RADIANT_INTENSITY,
-      RADIANCE,
-      IRRADIANCE,
-      RADIANT_EXITANCE,
-      RADIOSITY,
-      SPECTRAL_RADIANCE,
-      SPECTRAL_IRRADIANCE,
+      RU_RADIANT_ENERGY,
+      RU_RADIANT_FLUX,
+      RU_RADIANT_INTENSITY,
+      RU_RADIANCE,
+      RU_IRRADIANCE,
+      RU_RADIANT_EXITANCE,
+      RU_RADIOSITY,
+      RU_SPECTRAL_RADIANCE,
+      RU_SPECTRAL_IRRADIANCE,
       UNKNOWN_RADIANCE_UNIT
    };
 
@@ -228,15 +237,15 @@ public:
     */
    enum MassUnits
    {
-      METRIC_TON,
-      GRAM,
-      CARAT,
-      LONGTON,
-      SHORTON,
-      POUND,
-      OUNCE,
-      DRAM,
-      GRAIN,
+      MU_METRIC_TON,
+      MU_GRAM,
+      MU_CARAT,
+      MU_LONGTON,
+      MU_SHORTON,
+      MU_POUND,
+      MU_OUNCE,
+      MU_DRAM,
+      MU_GRAIN,
       UNKNOWN_MASS_UNIT
    };
 
@@ -246,9 +255,9 @@ public:
     */
    enum MagnetismUnits
    {
-      GAUSS,
-      TESLA,
-      OERSTED,
+      MAGU_GAUSS,
+      MAGU_TESLA,
+      MAGU_OERSTED,
       UNKNOWN_MAGNETISM_UNIT
    };
 
@@ -259,11 +268,11 @@ public:
     */
    enum PressureUnits
    {
-      POUNDS_PER_SQUARE_INCH,
-      ATMOSPHERIC_PRESSURE,
-      TORR,
-      BAR,
-      PASCAL,
+      PU_POUNDS_PER_SQUARE_INCH,
+      PU_ATMOSPHERIC_PRESSURE,
+      PU_TORR,
+      PU_BAR,
+      PU_PASCAL,
       UNKNOWN_PRESSURE_UNIT
    };
 
@@ -273,10 +282,10 @@ public:
     */
    enum TemperatureUnits
    {
-      FAHRENHEIT,
-      CELSIUS,
-      KELVIN,
-      RANKINE,
+      TU_FAHRENHEIT,
+      TU_CELSIUS,
+      TU_KELVIN,
+      TU_RANKINE,
       UNKNOWN_TEMPERATURE_UNIT
    };
 
@@ -286,15 +295,15 @@ public:
     */
    enum VolumeUnits
    {
-      FLUID_OUNCE,
-      PINT,
-      QUART,
-      GALLON,
-      CUBIC_INCHES,
-      CUBIC_FEET,
-      CUBIC_YARD,
-      LITER,
-      CUBIC_METER,
+      VU_FLUID_OUNCE,
+      VU_PINT,
+      VU_QUART,
+      VU_GALLON,
+      VU_CUBIC_INCHES,
+      VU_CUBIC_FEET,
+      VU_CUBIC_YARD,
+      VU_LITER,
+      VU_CUBIC_METER,
       UNKNOWN_VOLUME_UNIT
    };
 
@@ -412,8 +421,102 @@ public:
        TEMPERATURE,
        AMOUNT_OF_SUBSTANCE,
        LUMINOUS_INTENSITY,
+       DIMENSIONLESS,
        UNKNOWN_BASE_UNIT
    };
+
+   enum NamedDerivedUnits
+   {
+       FREQUENCY,
+       ANGLE,
+       SOLID_ANGLE,
+       FORCE,
+       PRESSURE,
+       ENERGY,
+       POWER,
+       ELECTRIC_CHARGE,
+       VOLTAGE,
+       CAPACITANCE,
+       RESISTANCE,
+       CONDUCTANCE,
+       MAGNETIC_FLUX,
+       MAGNETIC_FIELD,
+       INDUCTANCE,
+       LUMINOUS_FLUX,
+       ILLUMINANCE,
+       RADIOACTIVITY,
+       ABSORBED_DOSE,
+       EQUIVALENT_DOSE,
+       CATALYTIC_ACTIVITY,
+       EndNamedDerivedUnits
+   };
+
+   enum OtherDerivedUnits
+   {
+       AREA,
+       VOLUME,
+       VELOCITY,
+       VOLUMETRIC_FLOW,
+       ACCELERATION,
+       JERK,
+       SNAP,
+       ANGULAR_VELOCITY,
+       MOMENTUM,
+       ANGULAR_MOMENTUM,
+       TORQUE,
+       WAVENUMBER,
+       DENSITY,
+       SPECIFIC_VOLUME,
+       CONCENTRATION,
+       MOLAR_VOLUME,
+       HEAT_CAPACITY,
+       MOLAR_HEAT_CAPACITY,
+       SPECIFIC_HEAT_CAPACITY,
+       MOLAR_ENERGY,
+       SPECIFIC_ENERGY,
+       ENERGY_DENSITY,
+       SURFACE_TENSION,
+       IRRADIANCE,
+       THERMAL_CONDUCTIVITY,
+       KINEMATIC_VISCOSITY,
+       DYNAMIC_VISCOSITY,
+       ELECTRIC_CHARGE_DENSITY,
+       ELECTRIC_CURRENT_DESNTIY,
+       CONDUCTIVITY,
+       MOLAR_CONDUCTIVITY,
+       PERMITTIVITY,
+       PERMEABILITY,
+       ELECTRIC_FIELD_STRENGTH,
+       MAGNETIC_FIELD_STRENGTH,
+       LUMINANCE,
+       EXPOSURE,
+       ABSORBED_DOSE_RATE,
+       RESISTIVITY,
+       EndOtherDerivedUnits
+   };
+
+   std::string GetUnitType() const;
+   void SetUnitType(const std::string type);
+
+   std::string GetDisplayUnits() const;
+   void SetDisplayUnits(const std::string text);
+
+   Real GetConversionFactor() const;
+   void SetConversionFactor(Real factor);
+
+protected:
+
+    /// Variables in the unit class
+
+    std::string unitType;
+    std::string displayUnits;
+    Real conversionFactor;
+    StringArray numeratorUnits[7];
+    StringArray denominatorUnits[7];
+    IntegerArray numeratorPowers[7];
+    IntegerArray denominatorPowers[7];
+    RealArray numeratorPrefix[7];
+    RealArray denominatorPrefix[7];
 
     /// String Mappings for units
 
@@ -426,42 +529,77 @@ public:
     static const Real SI_PREFIXES_FACTOR[UNKNOWN_SI_PREFIX+1];
     static const std::string SI_PREFIXES_SYMBOL[UNKNOWN_SI_PREFIX+1];
     static const std::string SI_PREFIXES_STRING[UNKNOWN_SI_PREFIX+1];
+
+    static const Real ANGLE_UNITS_CONVERSION[UNKNOWN_ANGLE_UNIT+1];
     static const std::string ANGLE_UNITS_SYMBOL[UNKNOWN_ANGLE_UNIT+1];
     static const std::string ANGLE_UNITS_STRING[UNKNOWN_ANGLE_UNIT+1];
+
+    static const Real AREA_UNITS_CONVERSION[UNKNOWN_AREA_UNIT+1];
     static const std::string AREA_UNITS_SYMBOL[UNKNOWN_AREA_UNIT+1];
     static const std::string AREA_UNITS_STRING[UNKNOWN_AREA_UNIT+1];
+
+    static const Real DISTANCE_UNITS_CONVERSION[UNKNOWN_DISTANCE_UNIT+1];
     static const std::string DISTANCE_UNITS_SYMBOL[UNKNOWN_DISTANCE_UNIT+1];
     static const std::string DISTANCE_UNITS_STRING[UNKNOWN_DISTANCE_UNIT+1];
+
+    static const Real ELECTRIC_UNITS_CONVERSION[UNKNOWN_ELECTRIC_UNIT+1];
     static const std::string ELECTRIC_UNITS_SYMBOL[UNKNOWN_ELECTRIC_UNIT+1];
     static const std::string ELECTRIC_UNITS_STRING[UNKNOWN_ELECTRIC_UNIT+1];
+
+    static const Real ENERGY_UNITS_CONVERSION[UNKNOWN_ENERGY_UNIT+1];
     static const std::string ENERGY_UNITS_SYMBOL[UNKNOWN_ENERGY_UNIT+1];
     static const std::string ENERGY_UNITS_STRING[UNKNOWN_ENERGY_UNIT+1];
+
+    static const Real FORCE_UNITS_CONVERSION[UNKNOWN_FORCE_UNIT+1][UNKNOWN_FORCE_UNIT+1];
     static const std::string FORCE_UNITS_SYMBOL[UNKNOWN_FORCE_UNIT+1];
     static const std::string FORCE_UNITS_STRING[UNKNOWN_FORCE_UNIT+1];
+
+    static const Real LUMINANCE_UNITS_CONVERSION[UNKNOWN_LUMINANCE_UNIT+1];
     static const std::string LUMINANCE_UNITS_SYMBOL[UNKNOWN_LUMINANCE_UNIT+1];
     static const std::string LUMINANCE_UNITS_STRING[UNKNOWN_LUMINANCE_UNIT+1];
+
+    static const Real ILLUMINANCE_UNITS_CONVERSION[UNKNOWN_ILLUMINANCE_UNIT+1];
     static const std::string ILLUMINANCE_UNITS_SYMBOL[UNKNOWN_ILLUMINANCE_UNIT+1];
     static const std::string ILLUMINANCE_UNITS_STRING[UNKNOWN_ILLUMINANCE_UNIT+1];
+
+    static const Real LUMINOUS_FLUX_UNITS_CONVERSION[UNKNOWN_LUMINOUSFLUX_UNIT+1];
     static const std::string LUMINOUS_FLUX_UNITS_SYMBOL[UNKNOWN_LUMINOUSFLUX_UNIT+1];
     static const std::string LUMINOUS_FLUX_UNITS_STRING[UNKNOWN_LUMINOUSFLUX_UNIT+1];
+
+    static const Real LUMINOUS_ENERGY_UNITS_CONVERSION[UNKNOWN_LUMINOUSENERGY_UNIT+1];
     static const std::string LUMINOUS_ENERGY_UNITS_SYMBOL[UNKNOWN_LUMINOUSENERGY_UNIT+1];
     static const std::string LUMINOUS_ENERGY_UNITS_STRING[UNKNOWN_LUMINOUSENERGY_UNIT+1];
+
+    static const Real LUMINOUS_INTENSITY_UNITS_CONVERSION[UNKNOWN_LUMINOUSINTENSITY_UNIT+1];
     static const std::string LUMINOUS_INTENSITY_UNITS_SYMBOL[UNKNOWN_LUMINOUSINTENSITY_UNIT+1];
     static const std::string LUMINOUS_INTENSITY_UNITS_STRING[UNKNOWN_LUMINOUSINTENSITY_UNIT+1];
+
+    static const Real MASS_UNITS_CONVERSION[UNKNOWN_MASS_UNIT+1];
     static const std::string MASS_UNITS_SYMBOL[UNKNOWN_MASS_UNIT+1];
     static const std::string MASS_UNITS_STRING[UNKNOWN_MASS_UNIT+1];
+
+    static const Real PRESSURE_UNITS_CONVERSION[UNKNOWN_PRESSURE_UNIT+1];
     static const std::string PRESSURE_UNITS_SYMBOL[UNKNOWN_PRESSURE_UNIT+1];
     static const std::string PRESSURE_UNITS_STRING[UNKNOWN_PRESSURE_UNIT+1];
+
     /*
+    static const Real RADIANCE_UNITS_CONVERSION[UNKNOWN_RADIANCE_UNIT+1];
     static const std::string RADIANCE_UNITS_SYMBOL[UNKNOWN_RADIANCE_UNIT+1];
     static const std::string RADIANCE_UNITS_STRING[UNKNOWN_RADIANCE_UNIT+1];
     */
+
+    static const Real TEMPERATURE_UNITS_CONVERSION[UNKNOWN_TEMPERATURE_UNIT+1];
     static const std::string TEMPERATURE_UNITS_SYMBOL[UNKNOWN_TEMPERATURE_UNIT+1];
     static const std::string TEMPERATURE_UNITS_STRING[UNKNOWN_TEMPERATURE_UNIT+1];
+
+    static const Real VOLUME_UNITS_CONVERSION[UNKNOWN_VOLUME_UNIT+1];
     static const std::string VOLUME_UNITS_SYMBOL[UNKNOWN_VOLUME_UNIT+1];
     static const std::string VOLUME_UNITS_STRING[UNKNOWN_VOLUME_UNIT+1];
+
+    static const Real TIME_UNITS_CONVERSION[UNKNOWN_TIME_UNIT+1];
     static const std::string TIME_UNITS_SYMBOL[UNKNOWN_TIME_UNIT+1];
     static const std::string TIME_UNITS_STRING[UNKNOWN_TIME_UNIT+1];
+
     static const std::string MILITARY_TIME_ZONE_CODES[EndMilitaryTimeZones+1];
     static const Real MILITARY_TIME_UTC_OFFSETS[EndMilitaryTimeZones+1];
     static const std::string US_TIME_ZONE_CODES[EndUSTimeZones+1];
