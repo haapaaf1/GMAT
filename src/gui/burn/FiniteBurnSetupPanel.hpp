55ff636cf4df29c59937a31812525904cf3afa9a
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2009-03-11 18:01:20 +0000

    Removed all gui components except thruster selection ComboBox.

--- src/gui/burn/FiniteBurnSetupPanel.hpp
+++ src/gui/burn/FiniteBurnSetupPanel.hpp
@@ -1,4 +1,4 @@
-//$Header$
+//$Id$
 //------------------------------------------------------------------------------
 //                              FiniteBurnSetupPanel
 //------------------------------------------------------------------------------
@@ -16,54 +16,42 @@
 #define FiniteBurnSetupPanel_hpp
 
 #include "gmatwxdefs.hpp"
-#include "GuiInterpreter.hpp"
-#include "GuiItemManager.hpp"
-#include "GmatAppData.hpp"
 #include "GmatPanel.hpp"
 #include "FiniteBurn.hpp"
 
 class FiniteBurnSetupPanel : public GmatPanel
 {
 public:
-    FiniteBurnSetupPanel( wxWindow *parent, const wxString &burnName);
-    ~FiniteBurnSetupPanel();
+   FiniteBurnSetupPanel( wxWindow *parent, const wxString &burnName);
+   ~FiniteBurnSetupPanel();
 
 private:
-    static const int MAX_PROP_ROW = 5;
-
-    // member data
-    FiniteBurn *theBurn;
-
-    wxComboBox *mAxesComboBox;
-    //wxComboBox *mTankComboBox;
-    wxComboBox *mThrusterComboBox;
-    wxComboBox *mCentralBodyComboBox;
-//    wxGrid *engGrid;
-    wxTextCtrl *scaleTextCtrl;
-    
-    wxString thrusterSelected;
-    bool isThrusterEmpty;
-    
-    // member functions
-    void OnTextChange(wxCommandEvent& event);
-    void OnComboBoxChange(wxCommandEvent& event);
-
-    // methods inherited from GmatPanel
-    virtual void Create();
-    virtual void LoadData();
-    virtual void SaveData();
-
-    // any class wishing to process wxWindows events must use this macro
-    DECLARE_EVENT_TABLE();
-    
-    // IDs for the controls and the menu commands
-    enum
-    {     
-        ID_TEXT = 81000,
-        ID_TEXTCTRL,
-
-        ID_COMBOBOX
-    };
+   static const int MAX_PROP_ROW = 5;
+   
+   // member data
+   FiniteBurn *theBurn;
+   
+   wxComboBox *mThrusterComboBox;
+   
+   wxString thrusterSelected;
+   
+   // methods inherited from GmatPanel
+   virtual void Create();
+   virtual void LoadData();
+   virtual void SaveData();
+   
+   // for event handling
+   void OnComboBoxChange(wxCommandEvent& event);
+   
+   // any class wishing to process wxWindows events must use this macro
+   DECLARE_EVENT_TABLE();
+   
+   // IDs for the controls and the menu commands
+   enum
+   {     
+       ID_TEXT = 81000,
+       ID_COMBOBOX
+   };
 };
 
 #endif // FiniteBurnSetupPanel_hpp
