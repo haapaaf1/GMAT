36ad574b823023b4d020df617c54a289eaf42801
derickc <derickc@222415a0-0dc2-4184-858d-a27cde375526>
2009-07-16 15:13:07 +0000

    Committing MatLab files that generate GMAT and FreeFlyer Finite Burn Scripts

---
+++ RegSetupSVN/matlab/Generate_GMAT_FiniteBurn/Earth/FFFile.m
@@ -0,0 +1,12 @@
+function varargout = FFFile(filename,varargin);
+
+fid = fopen(filename,'w');
+
+for i = 1:nargin -1
+    cellfun(@(X) fprintf(fid,...
+        [regexprep(X,{'%','\\'},{'%%','\\\\'}),'\n']),varargin{i});
+end
+
+varargout{1} = fclose(fid);
+
+end
---
+++ RegSetupSVN/matlab/Generate_GMAT_FiniteBurn/Earth/GMATTestScript.m
@@ -0,0 +1,65 @@
+function varargout = GMATTestScript(Sc,Thrst,CSnum,Tnk);
+inputfolder = 'C:\Documents and Settings\DCrockett\Desktop\GMAT Repository\RegSetupSVN\input\AcceptTest\';
+outputfolder = '.\output\AcceptTest\';
+fname = ['FBurn_GMAT_','Earth_',Sc,'_',Thrst,'_CS',num2str(CSnum),'_',Tnk];
+
+
+%% Variable Scripts that Change
+Sc_String = GMATSpacecraftString(Sc);
+Thruster_String = GMATThrusterString(Thrst,CSnum);
+Tank_String = GMATTankString(Tnk);
+
+
+%%Constant script for producing results
+Script_Cell = {' ';
+'% -------------------------------------------------------------------------';
+'% --------------------------- Create Finite Burn  -------------------------';
+'% -------------------------------------------------------------------------';
+'Create FiniteBurn fb;';
+'GMAT fb.Thrusters = {engine1};';
+' ';
+'%  Create a force model';
+'Create ForceModel fm;';
+'GMAT fm.CentralBody = Earth;';
+'GMAT fm.PointMasses = {Earth};';
+'GMAT fm.Drag = None;';
+'GMAT fm.SRP = Off;';
+'GMAT fm.ErrorControl = RSSStep;';
+' ';
+'%  Create a propagator';
+'Create Propagator prop;';
+'GMAT prop.FM = fm;';
+'GMAT prop.Type = RungeKutta89;';
+'GMAT prop.InitialStepSize = 60;';
+'GMAT prop.Accuracy = 1e-9;';
+'GMAT prop.MinStep = 60;';
+'GMAT prop.MaxStep = 60;';
+'GMAT prop.MaxStepAttempts = 50;';
+' ';
+'%  Create a report file';
+'Create ReportFile rf;';
+['GMAT rf.Filename = ','''',[outputfolder,fname,'_output.report'],''';'];
+'GMAT rf.Precision = 16;';
+'GMAT rf.Add = {Sc.TAIModJulian, Sc.X, Sc.Y, Sc.Z, Sc.VX, Sc.VY, Sc.VZ, Sc.TotalMass};';
+'GMAT rf.WriteHeaders = Off;';
+'GMAT rf.ZeroFill = On;';
+'GMAT rf.ColumnWidth = 27;';
+'GMAT rf.LeftJustify = On;';
+' ';
+'% -------------------------------------------------------------------------';
+'% --------------------------- Mission Sequence ----------------------------';
+'% -------------------------------------------------------------------------';
+' ';
+'%  Turn on thrusters....they will remain on through all events until the';
+'%  "EndFiniteBurn fb(Sc)" command is executed.';
+'BeginFiniteBurn fb(Sc);';
+' ';
+'% Propagate for 2 hours, while the thrusters are turned on.';
+'Propagate prop(Sc,{Sc.ElapsedSecs = 7200});';
+' ';
+'%  Turn off thrusters   ';
+'EndFiniteBurn fb(Sc);';
+' ';
+' '};
+GMATFile([inputfolder,fname,'_testscript.m'],Sc_String,Thruster_String,Tank_String, Script_Cell);
+end
