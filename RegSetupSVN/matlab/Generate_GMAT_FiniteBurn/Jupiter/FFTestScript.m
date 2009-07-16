7a561e03e9f1768944965af2fb8dd641f5b46bee
derickc <derickc@222415a0-0dc2-4184-858d-a27cde375526>
2009-07-16 18:26:22 +0000

    Committing MatLab files that generate GMAT and FreeFlyer Finite Burn Scripts

---
+++ RegSetupSVN/matlab/Generate_GMAT_FiniteBurn/Jupiter/FFTestScript.m
@@ -0,0 +1,28 @@
+function varargout = FFTestScript(Sc,Thrst,CSnum,Tnk);
+inputfolder = 'C:\Documents and Settings\DCrockett\Desktop\GMAT Repository\RefSoftFiles\FreeFlyerData\FiniteBurn\AcceptTest\';
+outputfolder = 'C:\Documents and Settings\DCrockett\Desktop\GMAT Repository\RegSetupSVN\output\AcceptTest\Good_reports\FF\';
+fname = ['FBurn_FF_','Jupiter_',Sc,'_',Thrst,'_CS',num2str(CSnum),'_',Tnk];
+
+
+%% Variable Scripts that Change
+Sc_String = FFSpacecraftString(Sc);
+Thruster_String = FFThrusterString(Thrst,CSnum);
+Tank_String = FFTankString(Tnk);
+
+
+%%Constant script for producing results
+Script_Cell = {' ';
+' ';
+'fb.BurnDuration = prop.StepSize;  //Matches the fact that GMAT uses its propagator for maneuvering';
+' ';
+' ';
+'Variable i;';
+'For i = 0 to 7200 step fb.BurnDuration;';
+'Call FF_Match_GMAT_Report(Sc,FF_GMAT_Report);';
+'Maneuver Sc using fb;';
+'End;';
+' '};
+
+FFFile([inputfolder,fname,'_testscript.script'],FFPreamble([outputfolder,fname,'_output.report']),Sc_String,Tank_String,Thruster_String, Script_Cell);
+FFxmlWrapper([inputfolder,fname,'_testscript.script'],[inputfolder,fname,'_testscript.MissionPlan']);
+end
---
+++ RegSetupSVN/matlab/Generate_GMAT_FiniteBurn/Jupiter/GMATSpacecraftString.m
@@ -0,0 +1,42 @@
+function CellOut = GMATSpacecraftString(string_in);
+switch string_in
+    case 'ScA'
+Sc = {'%-----  Spacecraft A';    %Baseline Spacecraft
+        'Create Spacecraft Sc;';
+        'GMAT Sc.DateFormat = TAIModJulian;';
+        'GMAT Sc.Epoch = ''21545.000000000'';';
+        'GMAT Sc.CoordinateSystem = JupiterMJ2000Eq;';
+        'GMAT Sc.DisplayStateType = Keplerian;';
+        'GMAT Sc.SMA = 100000;';
+        'GMAT Sc.ECC = 0.0009999999999957077;';
+        'GMAT Sc.INC = 12.84999999999996;';
+        'GMAT Sc.RAAN = 306.6100000000001;';
+        'GMAT Sc.AOP = 314.1900000002609;';
+        'GMAT Sc.TA = 99.88769999973877;';
+        'GMAT Sc.DryMass = 850;';
+        'GMAT Sc.Cd = 2.2;';
+        'GMAT Sc.Cr = 1.8;';
+        'GMAT Sc.DragArea = 15;';
+        'GMAT Sc.SRPArea = 1;';
+        'GMAT Sc.Tanks = {tank1}';
+        'GMAT Sc.Thrusters = {engine1};';
+        'GMAT Sc.Attitude = CoordinateSystemFixed;';
+        'GMAT Sc.AttitudeDisplayStateType = ''Quaternion'';';
+        'GMAT Sc.AttitudeRateDisplayStateType = ''AngularVelocity'';';
+        'GMAT Sc.AttitudeCoordinateSystem = ''JupiterMJ2000Eq'';';
+        'GMAT Sc.Q1 = 0;';
+        'GMAT Sc.Q2 = 0;';
+        'GMAT Sc.Q3 = 0;';
+        'GMAT Sc.Q4 = 1;';
+        'GMAT Sc.EulerAngleSequence = ''312'';';
+        'GMAT Sc.AngularVelocityX = 0;';
+        'GMAT Sc.AngularVelocityY = 0;';
+        'GMAT Sc.AngularVelocityZ = 0;';
+        ' '};
+    otherwise
+        error(['Invalid Spacecraft Input (',string_in,')'])
+end
+
+    
+    CellOut = Sc;
+end
