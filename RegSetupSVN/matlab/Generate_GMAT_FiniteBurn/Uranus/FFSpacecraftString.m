e0388f5d0546ef9f378bb0450e65aa7da05cecd2
derickc <derickc@222415a0-0dc2-4184-858d-a27cde375526>
2009-07-21 12:50:02 +0000

    Committing Matlab files that Generate GMAT and FF Finite Burn scripts

---
+++ RegSetupSVN/matlab/Generate_GMAT_FiniteBurn/Uranus/FFSpacecraftString.m
@@ -0,0 +1,35 @@
+function CellOut = FFSpacecraftString(string_in);
+switch string_in
+    case 'ScA'
+        Sc = {'Create Spacecraft Sc(prop);';
+                'Sc.CentralBody = ''Uranus''; //Must match prop fm.PlanetFieldType[''CentralBody''] = 0;';
+                ' ';
+                '//State information';
+                'Sc.Epoch 		= 21545.000000000;';
+                'Sc.A       = 35000;';
+                'Sc.E       = 0.0009999999999957077;';
+                'Sc.I       = 12.84999999999996;';
+                'Sc.RAAN    = 306.6100000000001;';
+                'Sc.W       = 314.1900000002609;';
+                'Sc.TA      = 99.88769999973877;';
+                ' ';
+                '//Physical Information';
+                'Sc.VehicleDryMass 	= 850;';
+                'Sc.Cd 				= 2.2;';
+                'Sc.Cr				= 1.8;';
+                'Sc.DragArea 		= 15;';
+                'Sc.SRPArea 			= 1;';
+                ' ';
+                '//Attitude';
+                'Sc.Q1 = 0;';
+                'Sc.Q2 = 0;';
+                'Sc.Q3 = 0;';
+                'Sc.Q4 = 1;';
+                ' '};
+    otherwise
+        error(['Invalid Spacecraft Input (',string_in,')'])
+end
+
+    
+    CellOut = Sc;
+end
