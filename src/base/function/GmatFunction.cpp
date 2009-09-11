7f19c121f1b3d1eed50d5bf79c85ec1c262fac36
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2009-09-11 18:58:39 +0000

    Changed StartServer() to StartMatlabServer().

--- src/base/function/GmatFunction.cpp
+++ src/base/function/GmatFunction.cpp
@@ -343,7 +343,7 @@ bool GmatFunction::Initialize()
       
       // Check to see if the command needs a server startup (loj: 2008.07.25)
       if (current->NeedsServerStartup())
-         if (validator->StartServer(current) == false)
+         if (validator->StartMatlabServer(current) == false)
             throw FunctionException("Unable to start the server needed by the " +
                                    (current->GetTypeName()) + " command");
       
