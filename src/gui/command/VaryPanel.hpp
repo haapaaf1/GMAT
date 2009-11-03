f7f43b436adff7eb20d5956600e717506514af0d
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2009-11-03 21:18:39 +0000

    Updated VaryPanel to correctly save user input. Removed struct SolverType and mSolverData to simplify the code. Changed minimum/maximum to lower/upper. Debug change.

--- src/gui/command/VaryPanel.hpp
+++ src/gui/command/VaryPanel.hpp
@@ -29,41 +29,26 @@ class VaryPanel : public GmatPanel
 public:
    VaryPanel(wxWindow *parent, GmatCommand *cmd, bool inOptimize = false);
    ~VaryPanel(); 
-    
-private:             
    
-   struct SolverType
-   {
-      wxString  solverName;
-      wxString  varName;
-      wxString  initialValue;
-      wxString  pert;
-      wxString  minValue;
-      wxString  maxValue;
-      wxString  maxStep;
-      wxString  additiveScaleFactor;
-      wxString  multiplicativeScaleFactor;
-      //Real      initialValue;
-      //Real      pert;
-      //Real      minValue;
-      //Real      maxValue;
-      //Real      maxStep;
-      //Parameter *varParam;
-      //Real      additiveScaleFactor;
-      //Real      multiplicativeScaleFactor;
-   };
+protected:
+   Vary *mVaryCommand;
+   std::string solverName;
+   std::string variableName;
+   bool        inOptimizeCmd;
+   bool        solverChanged;
+   bool        variableChanged;
    
    wxTextCtrl *mVarNameTextCtrl;
    wxTextCtrl *mInitialTextCtrl;
    wxTextCtrl *mPertTextCtrl;
    wxTextCtrl *mMaxStepTextCtrl;
-   wxTextCtrl *mMinValueTextCtrl;
-   wxTextCtrl *mMaxValueTextCtrl;
+   wxTextCtrl *mLowerValueTextCtrl;
+   wxTextCtrl *mUpperValueTextCtrl;
    wxTextCtrl *mAdditiveTextCtrl;
    wxTextCtrl *mMultiplicativeTextCtrl;
 
-   wxStaticText *minValueStaticText;
-   wxStaticText *maxValueStaticText;
+   wxStaticText *lowerValueStaticText;
+   wxStaticText *upperValueStaticText;
    wxStaticText *pertStaticText;
    wxStaticText *maxStepStaticText;
    wxStaticText *additiveStaticText;
@@ -71,19 +56,13 @@ private:
    
    wxButton *mViewVarButton;
    wxComboBox *mSolverComboBox;
-
-   wxArrayString mObjectTypeList;
-   SolverType mSolverData;
-   Vary *mVaryCommand;
    
-   bool inOptimizeCmd;
+   wxArrayString mObjectTypeList;
    
    // methods inherited from GmatPanel
    virtual void Create();
    virtual void LoadData();
    virtual void SaveData();
-
-   void ShowVariableSetup();
    
    // event handling method
    void OnTextChange(wxCommandEvent& event);    
@@ -94,7 +73,7 @@ private:
    
    // any class wishing to process wxWindows events must use this macro
    DECLARE_EVENT_TABLE();
-  
+   
    // IDs for the controls and the menu commands
    enum
    {     
@@ -102,7 +81,6 @@ private:
       ID_TEXTCTRL,
       ID_BUTTON,
       ID_COMBO,
-      ID_GRID,
    };
 };
 
