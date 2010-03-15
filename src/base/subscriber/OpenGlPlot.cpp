43ebeffaf1e6a52979977ca24a1d71925a60fd55
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2010-03-15 19:49:58 +0000

    Called PlotInterface::TakeAction() to clear plotting objects in the destructor.

--- src/base/subscriber/OpenGlPlot.cpp
+++ src/base/subscriber/OpenGlPlot.cpp
@@ -421,10 +421,19 @@ OpenGlPlot& OpenGlPlot::operator=(const OpenGlPlot& ogl)
 
 
 //------------------------------------------------------------------------------
-// ~OpenGlPlot(void)
+// ~OpenGlPlot()
 //------------------------------------------------------------------------------
-OpenGlPlot::~OpenGlPlot(void)
-{   
+/**
+ * Destructor
+ *
+ * @note This destructor does not delete OpenGL plot window, but clears data.
+ *       OpenGL plot window is deleted when it is closed by the user or GMAT
+ *       shuts down.
+ */
+//------------------------------------------------------------------------------
+OpenGlPlot::~OpenGlPlot()
+{
+   PlotInterface::TakeGlAction(instanceName, "ClearObjects");
 }
 
 
