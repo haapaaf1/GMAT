47b2ba4b5f256fe8bf2310ec2a0715b8f49f9b24
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2010-04-07 20:01:45 +0000

    New factory to create FminconOptimizer

---
+++ plugins/FminconOptimizerPlugin/src/base/factory/FminconOptimizerFactory.hpp
@@ -0,0 +1,45 @@
+//$Id$
+//------------------------------------------------------------------------------
+//                            FminconOptimizerFactory
+//------------------------------------------------------------------------------
+// GMAT: General Mission Analysis Tool
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG06CA54C
+//
+// Author: Linda Jun
+// Created: 2010/03/30
+//
+/**
+ *  Declaration code for the FminconOptimizerFactory class.
+ */
+//------------------------------------------------------------------------------
+#ifndef FminconOptimizerFactory_hpp
+#define FminconOptimizerFactory_hpp
+
+
+#include "Factory.hpp"
+#include "Solver.hpp"
+
+class FminconOptimizerFactory : public Factory
+{
+public:
+   virtual Solver* CreateSolver(const std::string &ofType,
+                                const std::string &withName);
+   
+   // default constructor
+   FminconOptimizerFactory();
+   // constructor
+   FminconOptimizerFactory(StringArray createList);
+   // copy constructor
+   FminconOptimizerFactory(const FminconOptimizerFactory& fact);
+   // assignment operator
+   FminconOptimizerFactory& operator=(const FminconOptimizerFactory& fact);
+   
+   virtual ~FminconOptimizerFactory();
+   
+};
+
+#endif // FminconOptimizerFactory_hpp
