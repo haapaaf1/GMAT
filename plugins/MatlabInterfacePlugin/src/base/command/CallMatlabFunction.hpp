84ef8c6ce7c277b1f5b02ed3340d05db878d6712
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2010-04-07 17:35:11 +0000

    new class to support matlab function call

---
+++ plugins/MatlabInterfacePlugin/src/base/command/CallMatlabFunction.hpp
@@ -0,0 +1,58 @@
+//$Id$
+//------------------------------------------------------------------------------
+//                                 CallMatlabFunction
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P
+//
+// Author: Allison Greene
+// Created: 2004/09/22
+//
+/**
+ * Definition for the CallMatlabFunction command class
+ */
+//------------------------------------------------------------------------------
+#ifndef CallMatlabFunction_hpp
+#define CallMatlabFunction_hpp
+
+#include "CallFunction.hpp"
+#include "MatlabInterface.hpp"
+
+class CallMatlabFunction : public CallFunction
+{
+public:
+   CallMatlabFunction();
+   virtual ~CallMatlabFunction();
+   
+   CallMatlabFunction(const CallMatlabFunction& cf);
+   CallMatlabFunction&  operator=(const CallMatlabFunction& cf);
+   
+   std::string          FormEvalString();
+   
+   // override GmatCommand methods
+   virtual bool         Initialize();
+   virtual bool         Execute();
+   virtual void         RunComplete();
+   
+   // override GmatBase methods
+   virtual GmatBase*    Clone() const;
+
+protected:
+   
+   MatlabInterface *matlabIf;
+   
+   bool ExecuteMatlabFunction();
+   void SendInParam(Parameter *param);
+   void GetOutParams();
+   void EvalMatlabString(std::string evalString);
+   void ClearInputParameters();
+   void ClearOutputParameters();
+   void UpdateObject(GmatBase *obj, char *buffer);
+   
+};
+
+#endif // CallMatlabFunction_hpp
