389315d7b66db191030c4d10a1ff773c78f5951f
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2010-04-19 15:43:14 +0000

    Updated to derive from MdiChildViewFrame

--- src/gui/subscriber/MdiChildTrajFrame.hpp
+++ src/gui/subscriber/MdiChildTrajFrame.hpp
@@ -15,12 +15,9 @@
 #ifndef MdiChildTrajFrame_hpp
 #define MdiChildTrajFrame_hpp
 
-#include "GmatMdiChildFrame.hpp"
-#include "TrajPlotCanvas.hpp"
+#include "MdiChildViewFrame.hpp"
 #include "OpenGlOptionDialog.hpp"
-
 #include "gmatdefs.hpp"
-#include "SolarSystem.hpp"
 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
@@ -34,7 +31,7 @@
 #include "wx/mdi.h"
 #endif
 
-class MdiChildTrajFrame: public GmatMdiChildFrame
+class MdiChildTrajFrame: public MdiChildViewFrame
 {
 public:
    MdiChildTrajFrame(wxMDIParentFrame *parent, const wxString& plotName,
@@ -42,119 +39,15 @@ public:
                      const wxSize& size, const long style);
    virtual ~MdiChildTrajFrame();
    
-   // getters
-   wxString GetPlotName() { return mPlotName; }
-   bool GetOverlapPlot() { return mOverlapPlot; }
-   TrajPlotCanvas* GetGlCanvas() { return mCanvas; }
-   wxGLContext* GetGLContext() { return mCanvas->GetGLContext(); }
-   OpenGlOptionDialog* GetOptionDialog() { return mOptionDialog; }
-   bool GetUseViewPointInfo();
-   bool GetDrawWireFrame();
-   bool GetDrawXyPlane();
-   bool GetDrawEcPlane();
-   bool GetDrawSunLine();
-   bool GetDrawAxes();
-   bool GetDrawGrid();
-   UnsignedInt GetXyPlaneColor();
-   UnsignedInt GetSunLineColor();
-   Integer GetAnimationUpdateInterval();
-   Integer GetAnimationFrameIncrement();
+   virtual OpenGlOptionDialog* GetOptionDialog();
+   virtual void EnableAnimation(bool enable);
    
-   // setters
-   void SetSolarSystem(SolarSystem *ss);
-   void SetGLContext(wxGLContext *glContext = NULL);
-   void SetUserInterrupt();
-   
-   void SetPlotName(const wxString &name);
-   void ResetShowViewOption();
-   void SetOverlapPlot(bool overlap);
-   void SetUseInitialViewDef(bool flag);
-   void SetUsePerspectiveMode(bool flag);
-   void SetAnimationUpdateInterval(Integer value);
-   void SetAnimationFrameIncrement(Integer value);
-   void SetDrawWireFrame(bool flag);
-   void SetDrawXyPlane(bool flag);
-   void SetDrawEcPlane(bool flag);
-   void SetDrawSunLine(bool flag);
-   void SetDrawAxes(bool flag);
-   void SetDrawGrid(bool flag);
-   void SetXyPlaneColor(UnsignedInt color);
-   void SetSunLineColor(UnsignedInt color);
-   void SetObjectColors(const wxStringColorMap &objectColorMap);
-   void SetShowObjects(const wxStringBoolMap &showObjMap);
-   void SetShowOrbitNormals(const wxStringBoolMap &showOrbNormMap);
-   void SetNumPointsToRedraw(Integer numPoints);
-   
-   // actions
-   void DrawInOtherCoordSystem(const wxString &csName);
-   void RedrawPlot(bool viewAnimation);
-   
-   // menu actions
-   void OnClearPlot(wxCommandEvent& event);
-   void OnChangeTitle(wxCommandEvent& event);
-   void OnShowDefaultView(wxCommandEvent& event);
-
-   // from GL option dialog
-   void OnShowOptionDialog(wxCommandEvent& event);
-   void OnDrawWireFrame(wxCommandEvent& event);
-   void OnDrawXyPlane(wxCommandEvent& event);
-   
-   void OnQuit(wxCommandEvent& event);
-   
-   void OnActivate(wxActivateEvent& event);
-   void OnPlotSize(wxSizeEvent& event);
-   void OnMove(wxMoveEvent& event);
-   void OnPlotClose(wxCloseEvent &event);
-   void OnClose(wxCloseEvent &event);
-   
-   // drawing
-   void SetGlObject(const StringArray &objNames,
-                    const UnsignedIntArray &objOrbitColors,
-                    const std::vector<SpacePoint*> &objArray);
-   
-   void SetGlCoordSystem(CoordinateSystem *internalCs,
-                         CoordinateSystem *viewCs,
-                         CoordinateSystem *viewUpCs);
-   
-   void SetGlViewOption(SpacePoint *vpRefObj, SpacePoint *vpVecObj,
-                        SpacePoint *vdObj, Real vsFactor,
-                        const Rvector3 &vpRefVec, const Rvector3 &vpVec,
-                        const Rvector3 &vdVec, const std::string &upAxis,
-                        bool usevpRefVec, bool usevpVec, bool usevdVec,
-                        bool useFixedFov, Real fov);
-   
-   void SetGlDrawOrbitFlag(const std::vector<bool> &drawArray);
-   void SetGlShowObjectFlag(const std::vector<bool> &showArray);
-   void SetGlUpdateFrequency(Integer updFreq);
-   
-   void UpdatePlot(const StringArray &scNames, const Real &time,
-                   const RealArray &posX, const RealArray &posY,
-                   const RealArray &posZ, const RealArray &velX,
-                   const RealArray &velY, const RealArray &velZ,
-                   const UnsignedIntArray &scColors, bool solving,
-                   Integer solverOption, bool updateCanvas);
-   void TakeAction(const std::string &action);
-   void RefreshPlot();
-   void SetEndOfRun();
-   void DeletePlot();
-   
-   // OptionDialog
-   void EnableAnimation(bool enable);
+   virtual void OnShowOptionDialog(wxCommandEvent& event);
+   virtual void OnDrawWireFrame(wxCommandEvent& event);
    
 protected:
    
-   TrajPlotCanvas *mCanvas;
    OpenGlOptionDialog *mOptionDialog;
-   
-   wxString mPlotName;
-   wxString mPlotTitle;
-   bool mOverlapPlot;
-   
-   wxArrayString mBodyNames;
-   UnsignedIntArray mBodyColors;
-   
-   void CheckFrame();
-   
    DECLARE_EVENT_TABLE();
 };
 #endif
