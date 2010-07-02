7690ab749c742a70a52856a20141ee97b68d5f18
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2010-07-02 16:12:46 +0000

    Generated the build files for the MATLAB interface under (64-bit) Linux.  It all now works on that platform.  The Makefile changes are not tested on teh other platforms, so they should be checked there.

--- plugins/MatlabInterfacePlugin/build/linux64/MatlabInterfaceEnv.mk
+++ plugins/MatlabInterfacePlugin/build/linux64/MatlabInterfaceEnv.mk
@@ -5,35 +5,24 @@ GMAT_CODE_LOCATION = ../../../../src
 GMAT_BIN_LOCATION = ../../../../bin
 
 # location of MATLAB headers and libraries
-MATLAB_DIR = C:/Program\ Files/MATLAB/R2009B
+MATLAB_DIR = /opt/matlab/R2009b
 MATLAB_CPP_FLAGS = -D__USE_MATLAB__=1 -I$(MATLAB_DIR)/extern/include
-MATLAB_LIB_DIR = -L$(MATLAB_DIR)/bin/win32
+MATLAB_LIB_DIR = -L$(MATLAB_DIR)/bin/glnxa64
 MATLAB_LIBRARIES = $(MATLAB_LIB_DIR) -leng -lmx -lmat
 
 # Set to 0 for Windows, 1 for Linux or Mac
-LINUX_MAC = 0   
+LINUX_MAC = 1
 
 # Set to 0 for Linux, 1 for Mac 
 MAC_SPECIFIC = 0
 
-# Set to 1 to build against the MATLAB version, 0 for no MATLAB
-USE_MATLAB = 1
-
 DEBUG_BUILD = 0
 
 # Select the base library
-ifeq ($(USE_MATLAB), 0)
-BASE_LIBRARY = GmatBaseNoMatlab
-# BASE_LIB_LOCATION is only used on Mac
-BASE_LIB_LOCATION = GMATNoMatlab.app/Contents/Frameworks/
-else
 BASE_LIBRARY = GmatBase
-# BASE_LIB_LOCATION is only used on Mac
-BASE_LIB_LOCATION = GMAT.app/Contents/Frameworks/
-endif
 
 # DataFile library
-MATLAB_LIBRARY = DataFile
+#MATLAB_LIBRARY = DataFile
 
 # Compiler options
 CPP = g++
@@ -45,29 +34,8 @@ else
 PROFILE_FLAGS = 
 endif
 
-ifeq ($(LINUX_MAC), 1)
-
-ifeq ($(MAC_SPECIFIC), 1)
-SHARED_EXTENSION = .dylib
-SHARED_LIB_FLAGS = -bundle -two_levelnamespace -undefined dynamic_lookup \
-                   -L$(GMAT_CODE_LOCATION)/base/lib -l$(BASE_LIBRARY) \
-                    $(MATLAB_IF_LIBRARY) \
-                    $(MAC_CPP_FLAGS)
-                   
-
-else
 SHARED_EXTENSION = .so
 SHARED_LIB_FLAGS = -shared -Wl --out-implib
-endif
-
-else
-
-SHARED_EXTENSION = .dll
-SHARED_LIB_FLAGS = $(MATLAB_LINK_FLAGS) -shared -Wl --out-implib \
-                   -L$(GMAT_CODE_LOCATION)/base/lib -l$(BASE_LIBRARY) \
-                   $(MATLAB_LIBRARIES) \
-
-endif
 
 DESIRED_OPTIMIZATIONS =  -DSTRICT -Wall -fno-pcc-struct-return -O3 \
                  -finline-functions -funroll-loops -fno-rtti -DNO_GCC_PRAGMA \
@@ -75,16 +43,13 @@ DESIRED_OPTIMIZATIONS =  -DSTRICT -Wall -fno-pcc-struct-return -O3 \
                  -fexpensive-optimizations
 
 ifeq ($(DEBUG_BUILD), 1)
-OPTIMIZATIONS = -fno-strict-aliasing $(WX_28_DEFINES)
+OPTIMIZATIONS = -fno-strict-aliasing $(WX_28_DEFINES) -fPIC
 else
-OPTIMIZATIONS = -O3 -fno-strict-aliasing $(WX_28_DEFINES)
+OPTIMIZATIONS = -O3 -fno-strict-aliasing $(WX_28_DEFINES) -fPIC
 endif
-
-# wxWidgets flags
-WXCPPFLAGS = `/usr/local/bin/wx-config --cppflags`
 
 # Do not edit below this line -- here we build up longer compile/link strings
 CPP_BASE = $(OPTIMIZATIONS) -Wall \
-           $(MATLAB_CPP_FLAGS) $(WXCPPFLAGS) $(PROFILE_FLAGS) $(DEBUG_FLAGS)
+           $(MATLAB_CPP_FLAGS) $(PROFILE_FLAGS) $(DEBUG_FLAGS)
 
 CPPFLAGS = $(CPP_BASE)
