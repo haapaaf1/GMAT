211d5272d1880490152ce17e8ed6e25ac111f536
tgrubb <tgrubb@222415a0-0dc2-4184-858d-a27cde375526>
2010-10-13 20:59:21 +0000

    - Replaced LHS and RHS text with 'Left Hand Side' and 'Right Hand Side'

--- src/gui/controllogic/IfPanel.cpp
+++ src/gui/controllogic/IfPanel.cpp
@@ -100,7 +100,7 @@ void IfPanel::Setup(wxWindow *parent)
     
     conditionGrid->SetColLabelValue(0, _T(""));
     conditionGrid->SetColSize(0, 60);
-    conditionGrid->SetColLabelValue(1, _T("LHS"));
+    conditionGrid->SetColLabelValue(1, _T("Left Hand Side"));
     conditionGrid->SetColSize(1, 165);
     conditionGrid->SetColLabelValue(2, _T("Condition"));
 #ifdef __WXMAC__
@@ -108,7 +108,7 @@ void IfPanel::Setup(wxWindow *parent)
 #else
     conditionGrid->SetColSize(2, 60);
 #endif
-    conditionGrid->SetColLabelValue(3, _T("RHS"));
+    conditionGrid->SetColLabelValue(3, _T("Right Hand Side"));
     conditionGrid->SetColSize(3, 165);
     conditionGrid->SetCellValue(0, COMMAND_COL, "If");
         
