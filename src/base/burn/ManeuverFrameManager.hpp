7ab9d9f0f077e22f2c61dbf93d712f4e2a599028
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2010-10-27 20:37:05 +0000

    Changed Goddard -> General to make the files consistent, and changed Header -> Id so the svn tags are filled in

--- src/base/burn/ManeuverFrameManager.hpp
+++ src/base/burn/ManeuverFrameManager.hpp
@@ -1,8 +1,8 @@
-//$Header$
+//$Id$
 //------------------------------------------------------------------------------
 //                          ManeuverFrameManager
 //------------------------------------------------------------------------------
-// GMAT: Goddard Mission Analysis Tool
+// GMAT: General Mission Analysis Tool
 //
 // **Legal**
 //
