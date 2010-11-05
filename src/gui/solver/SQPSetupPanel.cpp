3414e352d9ec9efe94e79c7ae5a09c6b868b1a92
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2010-11-05 00:16:08 +0000

    Set the fmincon dialog to retrieve the path as well as file name when setting its report file location.  (Bug 2105)

--- src/gui/solver/SQPSetupPanel.cpp
+++ src/gui/solver/SQPSetupPanel.cpp
@@ -9,8 +9,8 @@
 // Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
 // number S-67573-G
 //
-// Author: 
-// Created: 
+// Author:
+// Created:
 //
 /**
  * Declares SQPSetupPanel.
@@ -46,7 +46,7 @@ END_EVENT_TABLE()
 //   "Off",
 //   "Notify",
 //   "Final"
-//}; 
+//};
 
 //------------------------------------------------------------------------------
 // SQPSetupPanel(wxWindow *parent, const wxString &name)
@@ -55,12 +55,12 @@ SQPSetupPanel::SQPSetupPanel(wxWindow *parent, const wxString &name)
    : GmatPanel(parent)
 {
    fileDialog = NULL;
-   
+
    theSolver =
       (Solver*)theGuiInterpreter->GetConfiguredObject(name.c_str());
 
    isTextModified = true;
-   
+
    if (theSolver != NULL)
    {
       Create();
@@ -103,53 +103,53 @@ void SQPSetupPanel::LoadData()
       mObject = theSolver;
 
 	  std::string valueStr;
-      
+
 // GradObj, GradConstr, DerivativeCheck, Diagnostics, and Display are no longer been used:
-//      std::string valueStr = theSolver->GetStringParameter("GradObj"); 
+//      std::string valueStr = theSolver->GetStringParameter("GradObj");
 //      if (valueStr == "On")
 //         gradObjCB->SetValue(true);
 //      else
 //         gradObjCB->SetValue(false);
-//         
-//      valueStr = theSolver->GetStringParameter("GradConstr"); 
+//
+//      valueStr = theSolver->GetStringParameter("GradConstr");
 //      if (valueStr == "On")
 //         gradConstrCB->SetValue(true);
 //      else
 //         gradConstrCB->SetValue(false);
-//         
-//      valueStr = theSolver->GetStringParameter("DerivativeCheck"); 
+//
+//      valueStr = theSolver->GetStringParameter("DerivativeCheck");
 //      if (valueStr == "On")
 //         derivativeCheckCB->SetValue(true);
 //      else
 //         derivativeCheckCB->SetValue(false);
-//      
+//
 //      valueStr = theSolver->GetStringParameter("Diagnostics");
 //      if (valueStr == "On")
 //         diagnosticsCB->SetValue(true);
 //      else
 //         diagnosticsCB->SetValue(false);
-         
-      valueStr = theSolver->GetStringParameter("TolFun"); 
+
+      valueStr = theSolver->GetStringParameter("TolFun");
       tolFunTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = theSolver->GetStringParameter("TolCon"); 
+
+      valueStr = theSolver->GetStringParameter("TolCon");
       tolConTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = theSolver->GetStringParameter("TolX"); 
+
+      valueStr = theSolver->GetStringParameter("TolX");
       tolXTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = theSolver->GetStringParameter("MaxFunEvals"); 
+
+      valueStr = theSolver->GetStringParameter("MaxFunEvals");
       maxFunEvalsTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = wxString::Format("%d",theSolver->GetIntegerParameter(theSolver->GetParameterID("MaximumIterations")));		//valueStr = theSolver->GetStringParameter("MaxIter"); 
+
+      valueStr = wxString::Format("%d",theSolver->GetIntegerParameter(theSolver->GetParameterID("MaximumIterations")));		//valueStr = theSolver->GetStringParameter("MaxIter");
       maxIterTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = theSolver->GetStringParameter("DiffMinChange"); 
+
+      valueStr = theSolver->GetStringParameter("DiffMinChange");
       diffMinChangeTextCtrl->SetValue(wxT(valueStr.c_str()));
-      
-      valueStr = theSolver->GetStringParameter("DiffMaxChange"); 
+
+      valueStr = theSolver->GetStringParameter("DiffMaxChange");
       diffMaxChangeTextCtrl->SetValue(wxT(valueStr.c_str()));
-     
+
 //      valueStr = theSolver->GetStringParameter("Display");
 //      displayComboBox->SetValue(wxT(valueStr.c_str()));
 
@@ -161,7 +161,7 @@ void SQPSetupPanel::LoadData()
 	  styleComboBox->SetValue(theSolver->GetStringParameter(id).c_str());
 
 	  id = theSolver->GetParameterID("ReportFile");
-	  reportfileTextCtrl->SetValue(theSolver->GetStringParameter(id).c_str());   
+	  reportfileTextCtrl->SetValue(theSolver->GetStringParameter(id).c_str());
    }
    catch (BaseException &e)
    {
@@ -169,7 +169,7 @@ void SQPSetupPanel::LoadData()
          ("SQPSetupPanel:LoadData() error occurred!\n%s\n",
           e.GetFullMessage().c_str());
    }
-   
+
    // explicitly disable apply button
    // it is turned on in each of the panels
    EnableUpdate(false);
@@ -180,14 +180,14 @@ void SQPSetupPanel::LoadData()
 // void SaveData()
 //------------------------------------------------------------------------------
 void SQPSetupPanel::SaveData()
-{   
+{
    canClose = true;
    Real tolFun, tolCon, tolX, diffMinChange, diffMaxChange;
    Integer maxIter, maxFunEvals;
    std::string tolFunStr, tolConStr, tolXStr, maxIterStr, maxFunEvalsStr;
    std::string diffMinChangeStr, diffMaxChangeStr;
    std::string str;
-   
+
    //-----------------------------------------------------------------
    // check values from text field
    //-----------------------------------------------------------------
@@ -196,41 +196,41 @@ void SQPSetupPanel::SaveData()
       str = tolFunTextCtrl->GetValue();
       CheckReal(tolFun, str, "TolFun", "Real Number > 0");
       tolFunStr = str;
-      
-      str = tolConTextCtrl->GetValue();      
+
+      str = tolConTextCtrl->GetValue();
       CheckReal(tolCon, str, "TolCon", "Real Number > 0");
       tolConStr = str;
-      
-      str = tolXTextCtrl->GetValue();      
+
+      str = tolXTextCtrl->GetValue();
       CheckReal(tolX, str, "TolX", "Real Number > 0");
       tolXStr = str;
-      
-      str = maxIterTextCtrl->GetValue();      
+
+      str = maxIterTextCtrl->GetValue();
       CheckInteger(maxIter, str, "MaximumIterations", "Integer Number > 0");
       maxIterStr = str;
-      
-      str = maxFunEvalsTextCtrl->GetValue();      
+
+      str = maxFunEvalsTextCtrl->GetValue();
       CheckInteger(maxFunEvals, str, "MaxFunEvals", "Integer Number > 0");
       maxFunEvalsStr = str;
-      
-      str = diffMinChangeTextCtrl->GetValue();      
+
+      str = diffMinChangeTextCtrl->GetValue();
       CheckReal(diffMinChange, str, "DiffMinChange", "Real Number > 0");
       diffMinChangeStr = str;
-      
-      str = diffMaxChangeTextCtrl->GetValue();      
+
+      str = diffMaxChangeTextCtrl->GetValue();
       CheckReal(diffMaxChange, str, "DiffMaxChange", "Real Number > 0");
       diffMaxChangeStr = str;
    }
-   
+
    if (!canClose)
       return;
-   
+
    try
    {
-      
+
       // Note: It will catch one error at a time
       // Is this acceptable?
-      
+
       if (isTextModified)
       {
          theSolver->SetStringParameter("TolFun", tolFunStr);
@@ -242,28 +242,28 @@ void SQPSetupPanel::SaveData()
          theSolver->SetStringParameter("DiffMaxChange", diffMaxChangeStr);
          isTextModified = false;
       }
-      
+
 // GradObj, GradConstr, DerivativeCheck, Diagnostics, and Display parameters are no loner been used:
 //      if (gradObjCB->IsChecked())
 //         theSolver->SetStringParameter("GradObj", "On");
 //      else
 //         theSolver->SetStringParameter("GradObj", "Off");
-//      
+//
 //      if (gradConstrCB->IsChecked())
 //         theSolver->SetStringParameter("GradConstr", "On");
 //      else
 //         theSolver->SetStringParameter("GradConstr", "Off");
-//      
+//
 //      if (derivativeCheckCB->IsChecked())
 //         theSolver->SetStringParameter("DerivativeCheck", "On");
 //      else
 //         theSolver->SetStringParameter("DerivativeCheck", "Off");
-//      
+//
 //      if (diagnosticsCB->IsChecked())
 //         theSolver->SetStringParameter("Diagnostics", "On");
 //      else
 //         theSolver->SetStringParameter("Diagnostics", "Off");
-//      
+//
 //      theSolver->SetStringParameter("Display", displayComboBox->GetValue().c_str());
 
 	  Integer id;
@@ -272,7 +272,7 @@ void SQPSetupPanel::SaveData()
 
 	  id = theSolver->GetParameterID("ReportStyle");
 	  theSolver->SetStringParameter(id, styleComboBox->GetValue().c_str());
-      
+
       id = theSolver->GetParameterID("ReportFile");
       theSolver->SetStringParameter(id, reportfileTextCtrl->GetValue().c_str());
    }
@@ -289,7 +289,7 @@ void SQPSetupPanel::SaveData()
 // void Setup( wxWindow *parent)
 //------------------------------------------------------------------------------
 void SQPSetupPanel::Setup( wxWindow *parent)
-{   
+{
 	wxString *styleArray = new wxString[4];
 	styleArray[0] = "Normal";
 	styleArray[1] = "Concise";
@@ -305,23 +305,23 @@ void SQPSetupPanel::Setup( wxWindow *parent)
 
 	// Max Iter
 	maxIterStaticText = new wxStaticText(parent, ID_TEXT, wxT("Max. Iterations"));
-	maxIterTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, 
+	maxIterTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition,
                      wxSize(100,-1));
 	// Max Fun Evals
 	maxFunEvalsStaticText = new wxStaticText(parent, ID_TEXT, wxT("Max. Func. Evals."));
-	maxFunEvalsTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, 
+	maxFunEvalsTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition,
                      wxSize(100,-1));
    // Tol X
    tolXStaticText = new wxStaticText(parent, ID_TEXT, wxT("Tol X"));
-   tolXTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, 
+   tolXTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition,
                      wxSize(100,-1));
    // Tol Con
    tolConStaticText = new wxStaticText(parent, ID_TEXT, wxT("Tol Con"));
-   tolConTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, 
+   tolConTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition,
                      wxSize(100,-1));
    // Tol Fun
    tolFunStaticText = new wxStaticText(parent, ID_TEXT, wxT("Tol Fun"));
-   tolFunTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, 
+   tolFunTextCtrl = new wxTextCtrl(parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition,
                      wxSize(100,-1));
    // Diff Max Change
    diffMaxChangeStaticText = new wxStaticText(parent, ID_TEXT, wxT("Diff Max Change"));
@@ -356,7 +356,7 @@ void SQPSetupPanel::Setup( wxWindow *parent)
 
     showProgressCheckBox = new wxCheckBox( parent, ID_CHECKBOX, wxT("Show Progress"),wxDefaultPosition, wxDefaultSize, 0 );
     reportStyleStaticText = new wxStaticText( parent, ID_TEXT, wxT("Report Style"), wxDefaultPosition,wxDefaultSize, 0);
-    styleComboBox = new wxComboBox( parent, ID_COMBOBOX, wxT("Normal"), wxDefaultPosition, 
+    styleComboBox = new wxComboBox( parent, ID_COMBOBOX, wxT("Normal"), wxDefaultPosition,
          wxSize(200,-1), 4, styleArray, wxCB_DROPDOWN|wxCB_READONLY );
     reportfileStaticText = new wxStaticText( parent, ID_TEXT, wxT("Report File"), wxDefaultPosition,wxDefaultSize, 0);
     reportfileTextCtrl = new wxTextCtrl( parent, ID_TEXTCTRL, wxT(""), wxDefaultPosition, wxSize(200,-1), 0 );
@@ -435,20 +435,20 @@ void SQPSetupPanel::OnBrowse(wxCommandEvent &event)
 		wxString defaultFile = wxEmptyString;
 		wxString wildcard = wxT("*.script");
 
-		fileDialog = new wxFileDialog(this, caption, 
-			defaultDir, defaultFile, 
-			wildcard, wxOPEN); 
+		fileDialog = new wxFileDialog(this, caption,
+			defaultDir, defaultFile,
+			wildcard, wxOPEN);
 	}
 	else
 	{
-		// show fileDialog when it exists 
+		// show fileDialog when it exists
 		fileDialog->Show();
 	}
 
 	if (fileDialog->ShowModal() == wxID_OK)
 	{
 		// change reportFile when a new file is chosen
-		wxString filename = fileDialog->GetFilename();
+		wxString filename = fileDialog->GetPath(); // To get full path to file
 		reportfileTextCtrl->SetValue(filename);
 		isTextModified = true;
 	}
