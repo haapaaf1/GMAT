e9fca52002ae30bd3338742d6d85b91da9e25049
wendys-dev <wendys-dev@222415a0-0dc2-4184-858d-a27cde375526>
2011-04-01 15:00:12 +0000

    Fixed loading of MatlabInterface dylib (missing -fno-rtti)

--- plugins/MatlabInterfacePlugin/build/mac_snowleopard/MatlabInterfaceEnv.mk
+++ plugins/MatlabInterfacePlugin/build/mac_snowleopard/MatlabInterfaceEnv.mk
@@ -85,9 +85,9 @@ DESIRED_OPTIMIZATIONS =  -DSTRICT -Wall -fno-pcc-struct-return -O3 \
                  -fexpensive-optimizations
 
 ifeq ($(DEBUG_BUILD), 1)
-OPTIMIZATIONS = -fno-strict-aliasing $(WX_28_DEFINES)
+OPTIMIZATIONS = -fno-strict-aliasing $(WX_28_DEFINES) -fno-rtti
 else
-OPTIMIZATIONS = -O3 -fno-strict-aliasing $(WX_28_DEFINES)
+OPTIMIZATIONS = -O3 -fno-strict-aliasing $(WX_28_DEFINES) -fno-rtti
 endif
 
 # Do not edit below this line -- here we build up longer compile/link strings
