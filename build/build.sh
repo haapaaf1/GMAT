1ec6449b84f24c64f27176188c6cd5ab3f6ca1d0
jpf200124 <jpf200124@222415a0-0dc2-4184-858d-a27cde375526>
2012-10-12 19:56:53 +0000

    Adding build.sh

---
+++ build/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Author: 	Jfisher
+# Project:	Gmat
+# Title:	configure.sh
+# Purpose:	Windows Script to configure and easily build gmat for end users
+# Usage: 	-arch [x86 | x64]
+
+# Clear the screen
+clear
+
+# Intro Text
+echo "........................................................"
+echo "Starting GMAT Build"
+echo "........................................................"
+echo
+
+# Set default variables
+arch="x86"
+
+# ***********************************
+# Input Args
+# ***********************************
+if [ "$1" = "-arch" ]
+then
+	arch=$2
+fi
+
+# ***********************************
+# Make File Generation and Build
+# ***********************************
+
+# Change to msw directory
+cd ../build/linux
+
+# Generate unix makefiles
+cmake -G "Unix Makefiles" ../../src/
+
+# Make Gmat
+make
+
+# Change back to build directory
+cd ../
+
+echo ""
+echo "*************************************"
+echo "Gmat Build Finished Succesfully!"
+echo "*************************************"
